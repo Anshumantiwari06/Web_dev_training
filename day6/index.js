328e7b697fe2ed44fd069df7520af16067d2b616
Anshuman Tiwari <tiwarianshuman085@gmail.com>
2024-12-02 12:39:52 +0530

    Add files via upload

---
+++ day6/index.js
@@ -0,0 +1,51 @@
+// // console.log('------Welcome in Js------')
+// // console.log("---------Variables in JS---------")
+// // var var1 = 5;
+// // var var2 = 6;
+// // var var3 = "helooooooo";
+// // var var4 = [2,5,74,56];
+// // console.log(var1,var2,var3,var4);
+// // let a = 5;
+// // let b = 6;
+// // let c= 7;
+// // let d = 8;
+// // let e = 99;
+// // let sum = a+b+c+d+e;
+// // console.log(sum);
+// // let average = (a+b+c+d+e)/5;
+// // console.log(average);
+// // let largest_number = Math.max(a,b,c,d,e);
+// // console.log(largest_number);
+//  let a = 10;
+//  let b = 20;
+//  let c = 15;
+
+// let largest;
+
+// if (a>= b && a >= c) {
+//     largest = a;
+// } else if (b >=a && b >= c) {
+//     largest = b;
+// } else {
+//     largest = c;
+// }
+
+// console.log("The largest number is: " + largest);
+let i;
+
+    for( i=3;i<100;i=i+3){
+        // if(i%3==0 && i%5==0 && i%9==0){
+            const tableline = " " + i + " = " + 10 * i;
+            console.log(tableline);
+        }
+        
+let ii = 0;
+let n = 10;
+while(ii<n){
+    console.log(ii);
+    ii++;
+}
+
+
+
+
